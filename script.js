5a57ae91b99b057fbdfe08e90fb605461c304909
Nagy-Bana Éva <nagybanaeva@gmail.com>
2020-06-17 23:08:32 +0200

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+var cats = {
+	'Selkirk Rex': 'göndör szőr', 
+	'Scottish Fold': 'gyűrött fül', 
+	'Exotic Shorthair': 'lapos orr'
+};
+
+
+document.write(cats['Selkirk Rex']);
+
+	
+cats.Biri = 'hosszú bajusz';
+
+document.write(cats.Biri);
+	
